9b81c5a7fdd1da09d163b2e23b7419185e8a5a08
noah.diderich <noah.diderich@student.kdg.be>
2024-07-09 14:27:07 +0200

    menu page improvement #13

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -9,17 +9,32 @@ interface MenuItem {
 }
 
 const menuItems: MenuItem[] = [
-    { imagePath: '/map-icon.png', cardName: 'Map', path: '/visitorInformation' },
-    { imagePath: '/ticket-icon.png', cardName: 'Ticket', path: '/tickets' },
-    { imagePath: '/gate-icon.png', cardName: 'Park Gates', path: '/parkGate' },
+    {imagePath: '/map-icon.png', cardName: 'Map', path: '/visitorInformation'},
+    {imagePath: '/ticket-icon.png', cardName: 'Ticket', path: '/tickets'},
+    {imagePath: '/gate-icon.png', cardName: 'Park Gates', path: '/parkGate'},
 ];
+
+const backgroundImageStyle = {
+    backgroundImage: `url(/techtopia-banner.jpg)`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    opacity: '0.7',
+};
 
 export default function Menu() {
     return (
-        <div className="menu-container">
-            {menuItems.map((item) => (
-                <MenuCard key={item.cardName} {...item} />
-            ))}
+        <div style={backgroundImageStyle}>
+            <div className="title">
+                <h1>Techtopia</h1>
+            </div>
+
+            <div className="menu-container">
+
+                {menuItems.map((item) => (
+                    <MenuCard key={item.cardName} {...item} />
+                ))}
+            </div>
         </div>
     );
 }
